ed102d43618d033854379a5bd498ed30af2d70cc
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 10:40:26 +0100

    correctiondone

--- sanitize.js
+++ sanitize.js
@@ -1,7 +1,8 @@
 // redo a sentance with proper characters
-let str = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
-let str1 = str.toLowerCase(); str.trim(); str.replace('\n', `\t`, ``);
+let str =
+  '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
 
+let str1 = str.toLowerCase(); str.trim();
 
 console.log(str1)
 
