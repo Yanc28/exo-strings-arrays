2672d98f1ae25bcf7718323a628f13778c780c0a
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 11:13:49 +0100

    correctiondone

--- translate.js
+++ translate.js
@@ -1,6 +1,21 @@
 let str =
   '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
 
-str1 = str.trim(); str.toUpperCase(); str.split(``);
+/*
+str
+  .trim()
+  .toUpperCase()
+  .split(' ')
+  .forEach((elem) => console.log(`mot: ${elem}`))
+*/
+
+
+// New method :
+
+
+str1 = str.trim().toUpperCase().split(` `)
+
+for (let elem of str1)
+  console.log(`mot : ${elem}`)
 
 console.log(str1)
