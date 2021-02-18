2af461efb9c57f8c548bb18dcfc920b19e3643bc
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 10:08:13 +0100

    correctiondone

---
+++ check.js
@@ -0,0 +1,13 @@
+let text = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript';
+let JS_text = 'Javascript';
+
+console.log(text)
+
+
+
+if (text.includes(JS_text)) {
+  console.log(`IL Y A BIEN LE MOT ${JS_text}`)
+} else {
+  console.log(`Le mot Javascript n'y est pas !!! `)
+}
+
