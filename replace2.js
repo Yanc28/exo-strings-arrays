2da829da94cca43f0a71f19770744c1a3719fdce
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 08:28:29 +0100

    done

---
+++ replace2.js
@@ -0,0 +1,9 @@
+str1 = `In cryptography, Alice and Bob are fictional characters commonly used as placeholders in discussions about cryptographic protocols or systems.
+As the use of Alice and Bob became more widespread, additional characters were added, sometimes each with a particular meaning.
+The most common characters are Alice and Bob. Eve, Mallory, and Trent are also common names.`
+
+// Replace "Alice" with "Yanis" in the whole text
+str2 = str1.replace(/Alice/gi, `Yanis`);
+
+console.log(str2)
+
