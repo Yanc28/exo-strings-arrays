408f7517ecb911191d6289ceb9079142c4c57886
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 08:27:35 +0100

    done

---
+++ check.js
@@ -0,0 +1,5 @@
+// Let's check if "Javascript" is mentionned in the sentence
+let pattern = 'Javascript'
+let str =
+  `\n\n\t\t JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript \n\n `
+
