d760feb00e6bff55f5be5845c8cb5f519e661edf
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 10:04:32 +0100

    correctiondone

--- check.js
+++ check.js
@@ -1,5 +1,13 @@
-// Let's check if "Javascript" is mentionned in the sentence
-let pattern = 'Javascript'
-let str =
-  `\n\n\t\t JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript \n\n `
+let text = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript';
+let JS_text = 'Javascript';
+
+console.log(text)
+
+
+
+if (text.includes(JS_text)) {
+  console.log(`IL Y A BIEN LE MOT ${JS_text}`)
+} else {
+  console.log(`Le mot Javascript n'y est pas !!! `)
+}
 
