9a2f378374f1a6e481e7a40f90cba2bd4eec3984
Yanc28 <yanisbouabdellahw@gmail.com>
2021-02-18 10:51:39 +0100

    correctiondone

---
+++ findBob.js
@@ -0,0 +1,16 @@
+
+let str = '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '
+
+str = str.trim()
+str = str.toLowerCase()
+
+console.log(str)
+
+if (str.includes(`bob`)) {
+  console.log(`bob exists`)
+} else {
+  console.log(`bob n\'est pas là`)
+}
+
+
+
